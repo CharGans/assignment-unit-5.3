00f6d3aee97a96cabbde01a3ae456e571e2bf50d
Charlotte Ganske <cganske04@icloud.com>
2024-12-08 17:19:03 -0600

    added js

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,11 +1,16 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+let myCollection = [];
 
+function addToCollection(collection, title, artist, yearPublished) {
+   collection.push(collection, title, artist, yearPublished);
+     return collection;
+}
 
+addToCollection(myCollection, 'Brat', 'Charli xcx', 2024)
 
-
-
+console.log(myCollection);
 
 
 
